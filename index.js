8775f38ecf86c63ba16da2820c2ad8bc92bcf071
AhmedAtef283 <aatef83156@gmail.com>
2026-09-23 17:20:58 +0300

    add Frontend

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
+require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const DB = require('./data/fetch.js');
-const app = express()
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 const coursesRoutes = require('./routes/courses.routes.js');
 
 app.use('/api/courses', coursesRoutes);
 
-
 app.listen(process.env.port, () => {
-    console.log(`Example app listening on localhost:${process.env.port}`)
+    console.log(`Example app listening on localhost:${process.env.port}`);
     DB.Connect();
-})
-
-
-
+});
